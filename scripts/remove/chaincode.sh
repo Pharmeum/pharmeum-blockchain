490f1030e399a27854d044924fd8e071df565854
Anfimov <uanavaro@gmail.com>
2019-07-04 15:46:26 +0300

    update  comand: removing paymain container and image is now alive

---
+++ scripts/remove/chaincode.sh
@@ -0,0 +1,19 @@
+# Copyright 2019 Pharmeum
+# Developed by Highchain Software https://highchain.io/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+#!/usr/bin/env bash
+
+docker rm -f dev-peer0.pharmeum.io-pharmeumccpayment-1.0-e4b165b9f71f5c321b3abffdc3f79ba153b45e4c266a8b9f018d479da6f11b51
+docker rmi -f dev-peer0.pharmeum.io-pharmeumccpayment-1.0-e4b165b9f71f5c321b3abffdc3f79ba153b45e4c266a8b9f018d479da6f11b51
